4b4be7ac4c8f10c82b534e7ed802473324a80521
ambition <taisiaiueo@gmail.com>
2015-10-03 13:37:34 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sage::Application.config.secret_key_base = '388ed5dc9c189080dd80740123d4837132810b91f26ac6e9e4723f9f89a95ee8377c5229bb62fd883b7e717501c8b59a6b543ffe841e2068bd9bc220519e7238'
