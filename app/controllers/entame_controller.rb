8fa49be4c50bff831450d2f65656b96a194e06c1
ambition <taisiaiueo@gmail.com>
2015-10-28 20:38:06 +0900

    all article change

--- app/controllers/entame_controller.rb
+++ app/controllers/entame_controller.rb
@@ -8,10 +8,10 @@ class EntameController < ApplicationController
     @genre = 'entame'
     uri = URI.parse('https://api.apigw.smt.docomo.ne.jp/webCuration/v3/contents?genreId=' + 2.to_s + '&s=1&n=50&APIKEY=427533784e4a4a38696b676a6b79472f516d616866654e6646746536554f383839663754744d496c793736')
     json = Net::HTTP.get(uri)
-    @results = JSON.parse(json)
-    for num in 1...@results['articleContents'].length
+    @entame_results = JSON.parse(json)
+    for num in 1...@entame_results['articleContents'].length
       #				puts results["articleContents"][num]["contentData"]["title"]
-      @results['articleContents'][num]['contentData']['linkUrl']
+      @entame_results['articleContents'][num]['contentData']['linkUrl']
     end
   end
 end
