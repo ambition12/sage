fd4bb817030d937027f0d8b6a030924d9c9e2c4b
Hirokazu <saigyoji205@gmail.com>
2015-10-29 16:44:05 +0900

    パラメータを渡すcontrollerとactionの変更

--- app/controllers/sage_controller.rb
+++ app/controllers/sage_controller.rb
@@ -1,4 +1,18 @@
 class SageController < ApplicationController
-	def top
+	#記事のグローバル配列(更新する際に配列の中身を変更し、リダイレクトする)
+	$url = ["http://www.google.com","http://www.google.com","http://www.google.com","http://www.google.com","http://www.google.com","http://www.google.com"]
+	def yourself
+	end
+	#記事更新用のaction
+	def yourself_update
+		render :text => "id=#{$url[params[:id].to_i]}"
+		case params[:id].to_i
+			when 1 then
+			when 2 then
+		        when 3 then
+			when 4 then
+			when 5 then
+		end	
+
 	end
 end
