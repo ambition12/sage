b9895a8b498f670306324b3626ccc0115c24a853
Hirokazu <saigyoji205@gmail.com>
2015-10-29 18:06:27 +0900

    タブをクリックするとサムネイル内の記事を更新

--- app/controllers/sage_controller.rb
+++ app/controllers/sage_controller.rb
@@ -5,14 +5,21 @@ class SageController < ApplicationController
 	end
 	#記事更新用のaction
 	def yourself_update
-		render :text => "id=#{$url[params[:id].to_i]}"
+#		render :text => "id=#{$url[params[:id].to_i]}"
 		case params[:id].to_i
 			when 1 then
+				$url = ["http://www.google.com","http://www.google.com","http://www.google.com","http://www.google.com","http://www.google.com","http://www.google.com"]
 			when 2 then
+				$url = ["http://www.yahoo.co.jp","http://www.yahoo.co.jp","http://www.yahoo.co.jp","http://www.yahoo.co.jp","http://www.yahoo.co.jp","http://www.yahoo.co.jp"]
 		        when 3 then
+				$url = ["http://www.google.com","http://www.google.com","http://www.google.com","http://www.google.com","http://www.google.com","http://www.google.com"]
 			when 4 then
+				$url = ["http://www.google.com","http://www.google.com","http://www.google.com","http://www.google.com","http://www.google.com","http://www.google.com"]
 			when 5 then
-		end	
-
+				$url = ["http://www.google.com","http://www.google.com","http://www.google.com","http://www.google.com","http://www.google.com","http://www.google.com"]
+			else
+				$url = ["http://www.google.com","http://www.google.com","http://www.google.com","http://www.google.com","http://www.google.com","http://www.google.com"]
+		end
+		redirect_to :action => "yourself"	
 	end
 end
