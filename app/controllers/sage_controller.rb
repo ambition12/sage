db77e2570e4f8465de673b074c5fba1c8e39b0e6
Hirokazu <saigyoji205@gmail.com>
2015-11-03 19:01:09 +0900

    null pointer対策

--- app/controllers/sage_controller.rb
+++ app/controllers/sage_controller.rb
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 class SageController < ApplicationController
 	#記事のグローバル配列(更新する際に配列の中身を変更し、リダイレクトする)
+	$default_url = "http://www.google.com"
 	$url = ["http://www.google.com","http://www.google.com","http://www.google.com","http://www.google.com","http://www.google.com","http://www.google.com"]
 	#$keyword(トレンド記事を検索するキーワード)
 	APIKEY = 'Av6GLD1hPwWcFziuhtlLqp6yCBXiZYGKJ8SXHox80dY'
@@ -13,29 +14,51 @@ class SageController < ApplicationController
 	def yourself_update
 		bing = Bing.new(APIKEY, 10, 'News')
 
-		case params[:id].to_i
-		when 1 then
-			results = bing.search($keyword[0])
-
-		when 2 then
-			results = bing.search($keyword[1])
-
-		when 3 then
-			results = bing.search($keyword[2])
-
-		when 4 then
-			results = bing.search($keyword[3])
-
-		when 5 then
-			results = bing.search($keyword[4])
-
+		pid = params[:id].to_i 
+		if pid <= $keyword.length then
+			results = bing.search($keyword[pid - 1])
+			for num in 0...6 do
+				articleUrl = results[0][:News][num][:Url]		
+				if articleUrl != nil then
+					$url[num] = articleUrl
+				end
+			end
 		else
-		end
-
-		for num in 0...6 do
-			$url[num] = results[0][:News][num][:Url]		
+			for num in 0...6 do
+				$url[num] = $default_url 
+			end
 		end
 
 		redirect_to :action => "yourself"
+
+		#====
+
+		#bing = Bing.new(APIKEY, 10, 'News')
+
+		#case params[:id].to_i
+		#when 1 then
+		#	results = bing.search($keyword[0])
+
+		#when 2 then
+		#	results = bing.search($keyword[1])
+
+		#when 3 then
+		#	results = bing.search($keyword[2])
+
+		#when 4 then
+		#	results = bing.search($keyword[3])
+
+		#when 5 then
+		#	results = bing.search($keyword[4])
+
+		#else
+		#	return
+		#end
+
+		#for num in 0...6 do
+		#	$url[num] = results[0][:News][num][:Url]		
+		#end
+
+		#redirect_to :action => "yourself"
 	end
 end
