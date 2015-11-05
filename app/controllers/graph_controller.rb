f8bb74488a2cc382f82db9f20fa3a1bd5e3b8032
ambition <taisiaiueo@gmail.com>
2015-11-05 14:12:04 +0900

    gragh

--- app/controllers/graph_controller.rb
+++ app/controllers/graph_controller.rb
@@ -1,14 +1,15 @@
 class GraphController < ApplicationController
-  def status_view category = [1,3,5,7]
-      current_quantity = [1000,5000,3000,8000]
+  def status_view
+    current_quantity = [1000, 5000, 3000, 8000]
+    days = [1, 3, 5, 7]
+    @graph = LazyHighCharts::HighChart.new('graph') do |f|
+      f.title(text: '月日を入れてくれ')
+      f.xAxis(categories: days, title: { text: '日付' })
+      f.series(name: '日にち', data: current_quantity)
+      f.chart(type: 'column')
 
-      @graph = LazyHighCharts::HighChart.new('graph') do |f|
-        f.title(text: '月日を入れてくれ')
-        f.xAxis(categories: category)
-        f.series(name: '日にち', data: current_quantity)
-
-    category = [1,3,5,7]
-    current_quantity = [1000,5000,3000,8000]
-  end
+      # days = [1, 3, 5, 7]
+      current_quantity = [1000, 5000, 3000, 8000]
+    end
 end
 end
