8c364cf08d048e2a00b2129c19b0cc4e85fc4bce
Tomonari <2bdi1106@mail.tokai-u.jp>
2015-11-10 21:01:45 +0900

    merge2

--- db/schema.rb
+++ db/schema.rb
@@ -63,4 +63,29 @@ ActiveRecord::Schema.define(version: 20151110104811) do
     t.datetime "updated_at"
   end
 
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "",          null: false
+    t.string   "encrypted_password",     default: "",          null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,           null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.integer  "failed_attempts",        default: 0,           null: false
+    t.string   "unlock_token"
+    t.datetime "locked_at"
+    t.datetime "created_at",                                   null: false
+    t.datetime "updated_at",                                   null: false
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "username",               default: "anonymous"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["unlock_token"], name: "index_users_on_unlock_token", unique: true
+
 end
