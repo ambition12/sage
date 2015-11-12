d8d7159e8c41ebe2bef88e41225658d78a1de97f
Tomonari <2bdi1106@mail.tokai-u.jp>
2015-11-12 18:00:22 +0900

    status正規化 @graph使用で使用可能

--- app/controllers/friend_controller.rb
+++ app/controllers/friend_controller.rb
@@ -9,22 +9,23 @@ class FriendController < ApplicationController
 
   def your_status
     your_name = params[:your_name]
+    genre = ['ゲーム', 'アニメ', '政治・経済', 'エンタメ', 'スポーツ', 'IT・科学', 'ライフ', '旅行', 'グルメ']
 
     if Status.find_by(username: your_name).nil?
-      @your_status = Array.new(9, 0)
+      your_status = Array.new(9, 0)
       @your_trend = Array.new(5, "google")
     else
       your_status_tmp = Status.find_by(username: your_name)
-      @your_status = Array.new(9,0)
-      @your_status[0] = your_status_tmp.game
-      @your_status[1] = your_status_tmp.anime
-      @your_status[2] = your_status_tmp.economy
-      @your_status[3] = your_status_tmp.entame
-      @your_status[4] = your_status_tmp.sports
-      @your_status[5] = your_status_tmp.tech
-      @your_status[6] = your_status_tmp.life
-      @your_status[7] = your_status_tmp.tour
-      @your_status[8] = your_status_tmp.gourmet
+      your_status = Array.new(9,0)
+      your_status[0] = your_status_tmp.game
+      your_status[1] = your_status_tmp.anime
+      your_status[2] = your_status_tmp.economy
+      your_status[3] = your_status_tmp.entame
+      your_status[4] = your_status_tmp.sports
+      your_status[5] = your_status_tmp.tech
+      your_status[6] = your_status_tmp.life
+      your_status[7] = your_status_tmp.tour
+      your_status[8] = your_status_tmp.gourmet
 
       your_trend_tmp = MyTrend.find_by(username: your_name)
       @your_trend = Array.new(5,"google")
@@ -34,7 +35,33 @@ class FriendController < ApplicationController
       @your_trend[3] = your_trend_tmp.four
       @your_trend[4] = your_trend_tmp.five
     end
-    # redirect_to action: :show, url: url
+
+    aData = Array.new(your_status.length, 0)
+
+    max = your_status.max
+
+    your_status.each_with_index do |count, index|
+      if (max != 0)
+        aData[index] = 1000 * count / max
+      else
+        aData[index] = 0
+      end
+    end
+
+    @graph = LazyHighCharts::HighChart.new('graph') do |f|
+      f.chart(polar: true, type: 'line') # グラフの種類
+      f.pane(size: '100%') # グラフサイズの比
+      f.title(text: 'Status') # タイトル
+      f.xAxis(categories: genre, tickmarkPlacement: 'on')
+      # categories:各項目の名前,tickmarkPlacement:'on'だとメモリ表示がカテゴリーの表示に沿う
+      f.yAxis(gridLineInterpolation: 'polygon', lineWidth: 0, min: 0, max: 1000) # 各項目の最大値やら
+      f.series(name: 'ジャンル', data: @aData, pointPlacement: 'on')
+      # 各データ
+      f.legend(align: 'right',
+               verticalAlign: 'top',
+               y: 70,
+               layout: 'vertical')
+    end
   end
 
   def follow
