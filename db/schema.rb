0598c2546a67ae3847182ba04832e95d34678de8
Tomonari <2bdi1106@mail.tokai-u.jp>
2015-11-13 15:20:43 +0900

    正規表現実装 rake db:migrate:reset

--- db/schema.rb
+++ db/schema.rb
@@ -52,7 +52,6 @@ ActiveRecord::Schema.define(version: 20151110104811) do
     t.integer  "tour"
     t.integer  "gourmet"
     t.string   "major"
-    t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
